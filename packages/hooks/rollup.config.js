2c22943b6af48a72ffb29b8a76ddcd9cbff52f45
Etroynov <troinof@yandex.ru>
2020-06-18 23:45:15 +0300

    feat(config): improve rollup config

--- packages/hooks/rollup.config.js
+++ packages/hooks/rollup.config.js
@@ -1,12 +1,11 @@
 import ts from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 import { terser as uglify } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
-    format: 'esm'
+    format: 'cjs'
   },
 
   plugins: [ts(), uglify()]
