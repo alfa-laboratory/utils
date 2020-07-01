e947e42b2657f191aa02303eb7cea5c99b9867a1
Etroynov <troinof@yandex.ru>
2020-07-01 11:22:36 +0300

    feat(add-data-pkg): add currency data pkg

---
+++ packages/data/rollup.config.js
@@ -0,0 +1,12 @@
+import ts from '@rollup/plugin-typescript';
+import { terser as uglify } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+
+  plugins: [ts(), uglify()]
+}
