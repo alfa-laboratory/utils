ec23da56d18ec2196ed8e0f4d5140c3a612314dd
Etroynov <troinof@yandex.ru>
2020-08-17 12:30:38 +0300

    chore(*): release 0.3.0

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
-export * from './pluralize';
+export * from './format-account';
 export * from './format-amount';
 export * from './get-all-currency-codes';
 export * from './get-currency-symbol';
+export * from './pluralize';
