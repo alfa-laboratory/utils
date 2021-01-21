309cde1e01f48781cc7a7d9bdb4adb75ddb7ad9c
aleksey-ilin <ilinalekseys@gmail.com>
2021-01-21 12:59:37 +0300

    feat(format-phone-util): add test, edit util

--- packages/utils/src/format-phone/util.test.ts
+++ packages/utils/src/format-phone/util.test.ts
@@ -4,6 +4,9 @@ describe('getRawPhoneNumber', () => {
     it('should return raw phone number', () => {
         expect(getRawPhoneNumber('+7 111 222-33-44')).toBe('71112223344');
         expect(getRawPhoneNumber('-7 111 222-33-44/z')).toBe('71112223344');
+        expect(getRawPhoneNumber('-7 (111) 222-33-44')).toBe('71112223344');
+        expect(getRawPhoneNumber('8 800 222-33-44')).toBe('88002223344');
+        expect(getRawPhoneNumber('8 (800) 222-33-44/z')).toBe('88002223344');
     });
 });
 
@@ -11,5 +14,7 @@ describe('formatPhoneNumber', () => {
     it('should return formatted phone number by mask', () => {
         expect(formatPhoneNumber('71112223344')).toBe('+7 111 222-33-44');
         expect(formatPhoneNumber('7 111 222 33 44')).toBe('+7 111 222-33-44');
+        expect(formatPhoneNumber('88002223344')).toBe('8 800 222-33-44');
+        expect(formatPhoneNumber('8 800 222 33 44')).toBe('8 800 222-33-44');
     });
 });
