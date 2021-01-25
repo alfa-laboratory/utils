a51d1070cbc6ee1fb7d7ae3762de94fe5a91d306
aleksey-ilin <ilinalekseys@gmail.com>
2021-01-25 12:22:59 +0300

    improvement(format-phone): improvement after code-review

--- packages/utils/src/format-phone/util.test.ts
+++ packages/utils/src/format-phone/util.test.ts
@@ -1,20 +1,20 @@
-import { getRawPhoneNumber, formatPhoneNumber } from './util';
+import { phoneNumber } from './util';
 
-describe('getRawPhoneNumber', () => {
+describe('phoneNumber.getRaw', () => {
     it('should return raw phone number', () => {
-        expect(getRawPhoneNumber('+7 111 222-33-44')).toBe('71112223344');
-        expect(getRawPhoneNumber('-7 111 222-33-44/z')).toBe('71112223344');
-        expect(getRawPhoneNumber('-7 (111) 222-33-44')).toBe('71112223344');
-        expect(getRawPhoneNumber('8 800 222-33-44')).toBe('88002223344');
-        expect(getRawPhoneNumber('8 (800) 222-33-44/z')).toBe('88002223344');
+        expect(phoneNumber.getRaw('+7 111 222-33-44')).toBe('71112223344');
+        expect(phoneNumber.getRaw('-7 111 222-33-44/z')).toBe('71112223344');
+        expect(phoneNumber.getRaw('-7 (111) 222-33-44')).toBe('71112223344');
+        expect(phoneNumber.getRaw('8 800 222-33-44')).toBe('88002223344');
+        expect(phoneNumber.getRaw('8 (800) 222-33-44/z')).toBe('88002223344');
     });
 });
 
-describe('formatPhoneNumber', () => {
-    it('should return formatted phone number by mask', () => {
-        expect(formatPhoneNumber('71112223344')).toBe('+7 111 222-33-44');
-        expect(formatPhoneNumber('7 111 222 33 44')).toBe('+7 111 222-33-44');
-        expect(formatPhoneNumber('88002223344')).toBe('8 800 222-33-44');
-        expect(formatPhoneNumber('8 800 222 33 44')).toBe('8 800 222-33-44');
+describe('phoneNumber.format', () => {
+    it('should return formatted phone number', () => {
+        expect(phoneNumber.format('71112223344')).toBe('+7 111 222-33-44');
+        expect(phoneNumber.format('7 111 222 33 44')).toBe('+7 111 222-33-44');
+        expect(phoneNumber.format('88002223344')).toBe('8 800 222-33-44');
+        expect(phoneNumber.format('8 800 222 33 44')).toBe('8 800 222-33-44');
     });
 });
