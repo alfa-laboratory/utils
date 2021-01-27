02edbf408617902786dc0f604743f44bf6603349
Alexander Soldatov <aesoldatov@alfabank.ru>
2021-01-27 14:22:09 +0300

    fix(seconds-to-time): use custom pad function instead of internal

--- packages/utils/src/seconds-to-time/util.ts
+++ packages/utils/src/seconds-to-time/util.ts
@@ -7,8 +7,12 @@ export function secondsToTime(seconds: number) {
     const s = seconds - h * 3600 - m * 60;
 
     return {
-        hours: `${h}`.padStart(2, '0'),
-        minutes: `${m}`.padStart(2, '0'),
-        seconds: `${s}`.padStart(2, '0'),
+        hours: `${numPad('00', h)}`,
+        minutes: `${numPad('00', m)}`,
+        seconds: `${numPad('00', s)}`,
     };
+}
+
+function numPad(pad: string, num: number) {
+    return typeof num === 'undefined' ? pad : (pad + num).slice(-pad.length);
 }
