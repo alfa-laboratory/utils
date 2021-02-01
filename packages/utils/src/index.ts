04c12ddae2e8d006f6fe6fd7e7355dd23022d78c
stepancar <stepancar@hotmail.com>
2021-02-01 06:13:25 +0300

    feat(utils): add export

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './format-account';
+export * from './format-amount';
 export * from './split-amount';
 export * from './get-all-currency-codes';
 export * from './get-currency-symbol';
