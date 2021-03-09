5affe717d2ac959a603e17c3b0f78f3337a01326
Dmitry Savkin <dmitrsavk@yandex.ru>
2021-03-09 08:34:52 +0300

    feat(utils): fixes
    Co-authored-by: Alexander Soldatov <inc1uder@ya.ru>

--- packages/utils/src/format-phone/mask/util.test.ts
+++ packages/utils/src/format-phone/mask/util.test.ts
@@ -1,6 +1,6 @@
 import { maskPhone } from '.';
 
-describe('formatPhoneNumber', () => {
+describe('maskPhone', () => {
     it('should return formatted phone number', () => {
         expect(maskPhone('+7 111 222-33-44')).toBe('+7 ··· ··· 33-44');
     });
