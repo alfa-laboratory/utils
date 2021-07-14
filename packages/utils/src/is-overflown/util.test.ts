c5af584669e502e93f0468a05cd2fd0784358cde
Alexander Soldatov <aesoldatov@alfabank.ru>
2021-07-14 19:23:44 +0300

    feat(utils): add isOverflown

---
+++ packages/utils/src/is-overflown/util.test.ts
@@ -0,0 +1,51 @@
+import { isOverflown } from './';
+
+describe('isOverflown', () => {
+    it('should return `true` if element content height overflowing', () => {
+        const container = document.createElement('section');
+
+        jest.spyOn(container, 'clientHeight', 'get').mockImplementation(() => 10);
+        jest.spyOn(container, 'scrollHeight', 'get').mockImplementation(() => 20);
+
+        expect(isOverflown(container)).toEqual(true);
+    });
+
+    it('should return `false` if element content height equals or less than container', () => {
+        const container = document.createElement('section');
+
+        jest.spyOn(container, 'clientHeight', 'get').mockImplementation(() => 10);
+        jest.spyOn(container, 'scrollHeight', 'get').mockImplementation(() => 10);
+
+        expect(isOverflown(container)).toEqual(false);
+
+        jest.spyOn(container, 'scrollHeight', 'get').mockImplementation(() => 0);
+
+        expect(isOverflown(container)).toEqual(false);
+    });
+
+    it('should return `true` if element content width overflowing', () => {
+        const container = document.createElement('section');
+
+        jest.spyOn(container, 'clientWidth', 'get').mockImplementation(() => 10);
+        jest.spyOn(container, 'scrollWidth', 'get').mockImplementation(() => 20);
+
+        expect(isOverflown(container)).toEqual(true);
+    });
+
+    it('should return `false` if element content width equals or less than container', () => {
+        const container = document.createElement('section');
+
+        jest.spyOn(container, 'clientWidth', 'get').mockImplementation(() => 10);
+        jest.spyOn(container, 'scrollWidth', 'get').mockImplementation(() => 10);
+
+        expect(isOverflown(container)).toEqual(false);
+
+        jest.spyOn(container, 'scrollWidth', 'get').mockImplementation(() => 0);
+
+        expect(isOverflown(container)).toEqual(false);
+    });
+
+    it('should throw an error if called without an arg', () => {
+        expect(isOverflown).toThrow("Cannot read property 'scrollHeight' of undefined");
+    });
+});
