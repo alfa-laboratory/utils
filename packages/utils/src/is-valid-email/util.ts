ade4e102642727e834a9ce374696271001818739
Mikhail Koretsky <koretsky97@gmail.com>
2021-11-16 13:00:51 +0300

    fix(is-valid-email): codestyle

--- packages/utils/src/is-valid-email/util.ts
+++ packages/utils/src/is-valid-email/util.ts
@@ -1,3 +1,5 @@
+const EMAIL_PARTS_SEPARATOR = '@';
+const DOMAIN_PARTS_SEPARATOR = '.';
 const MAX_ACCOUNT_LENGTH = 64;
 const MAX_ADDRES_LENGTH = 255;
 const MAX_DOMAIN_LENGTH = 63;
@@ -12,22 +14,21 @@ const EMAIL_REGEX = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a
 export function isValidEmail(email: string): boolean {
   if (!email) return false;
 
-  const emailParts = email.split('@');
+  const emailParts = email.split(EMAIL_PARTS_SEPARATOR);
 
-  if (emailParts.length !== 2) return false
+  if (emailParts.length !== 2) return false;
 
   const [account, address] = emailParts;
 
   const accountTooLong = account.length > MAX_ACCOUNT_LENGTH;
   const addressTooLong = address.length > MAX_ADDRES_LENGTH;
 
-  if (accountTooLong || addressTooLong) return false
+  if (accountTooLong || addressTooLong) return false;
 
-  const domainParts = address.split('.');
-  const domainTooLong = domainParts.some((part) => part.length > MAX_DOMAIN_LENGTH)
+  const domainParts = address.split(DOMAIN_PARTS_SEPARATOR);
+  const domainTooLong = domainParts.some((part) => part.length > MAX_DOMAIN_LENGTH);
 
   if (domainTooLong) return false;
 
-
-  return EMAIL_REGEX.test(email)
+  return EMAIL_REGEX.test(email);
 };
