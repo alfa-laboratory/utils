cc4768bccca991ee139f1d2e8889df5ea4cadeb7
Mikhail Koretsky <koretsky97@gmail.com>
2021-12-28 10:05:28 +0300

    fix(use-force-update): formatting

--- packages/hooks/src/useForceUpdate/hook.test.tsx
+++ packages/hooks/src/useForceUpdate/hook.test.tsx
@@ -1,65 +1,70 @@
 import * as React from 'react';
 import { render, act } from '@testing-library/react';
 
-import useForceUpdate from './hook';
+import {useForceUpdate} from './hook';
 
 describe('useForceUpdate', () => {
-  const forceUpdates: Array<() => void> = [];
-  let renders: number;
-  let TestComponent: React.FunctionComponent<{}> = () => null;
+    const forceUpdates: Array<() => void> = [];
+    let renders: number;
+    let TestComponent: React.FunctionComponent<{}> = () => null;
 
-  beforeEach((): void => {
-    forceUpdates.splice(0, forceUpdates.length);
+    beforeEach(() => {
+        forceUpdates.splice(0, forceUpdates.length);
 
-    renders = 0;
+        renders = 0;
 
-    TestComponent = (): null => {
-      forceUpdates.push(useForceUpdate());
-      renders++;
-      return null;
-    };
+        TestComponent = (): null => {
+            forceUpdates.push(useForceUpdate());
+            renders++;
+            return null;
+        };
+    });
 
-  });
+    it('should accept no parameters', () => {
+        render(<TestComponent />);
 
-  it('should accept no parameters', (): void => {
-    render(<TestComponent />);
-    
-    expect(forceUpdates[0].length).toEqual(0);
-  });
+        expect(forceUpdates[0].length).toEqual(0);
+    });
 
-  it('should maintain the same reference', (): void => {
-    render(<TestComponent />);
-    
-    act(() => {
-      forceUpdates[0]();
-    })
-    
-    expect(forceUpdates[0]).toEqual(forceUpdates[1]);
-  });
+    it('should maintain the same reference', () => {
+        render(<TestComponent />);
 
-  it('should return undefined', (): void => {
-    render(<TestComponent />);
+        act(() => {
+            forceUpdates[0]();
+        });
 
-    let hookReturnValue
+        expect(forceUpdates[0]).toEqual(forceUpdates[1]);
+    });
 
-    act(() => {
-      hookReturnValue = forceUpdates[0]()
-    })
+    it('should return undefined', () => {
+        render(<TestComponent />);
 
-    expect(hookReturnValue).toBeUndefined();
-  });
+        let hookReturnValue;
 
-  it('should update the component', (): void => {
-    expect(renders).toEqual(0);
-    render(<TestComponent />);
-    expect(renders).toEqual(1);
-    act(() => {
-    forceUpdates[0]();
-    })
-    expect(renders).toEqual(2);
-    act(() => {
-    forceUpdates[1]();
-    })
-    expect(renders).toEqual(3);
-  });
+        act(() => {
+            hookReturnValue = forceUpdates[0]();
+        });
+
+        expect(hookReturnValue).toBeUndefined();
+    });
+
+    it('should update the component', () => {
+        expect(renders).toEqual(0);
+
+        render(<TestComponent />);
+
+        expect(renders).toEqual(1);
+
+        act(() => {
+            forceUpdates[0]();
+        });
+
+        expect(renders).toEqual(2);
+
+        act(() => {
+            forceUpdates[1]();
+        });
+        
+        expect(renders).toEqual(3);
+    });
 });
--- packages/hooks/src/useForceUpdate/hook.ts
+++ packages/hooks/src/useForceUpdate/hook.ts
@@ -1,14 +1,11 @@
 import { useCallback, useState } from 'react';
 
-export default function useForceUpdate(): () => void {
-  const [ , dispatch ] = useState<{}>(Object.create(null));
+export function useForceUpdate() {
+    const [, dispatch] = useState<{}>(Object.create(null));
 
-  const memoizedDispatch = useCallback(
-    (): void => {
-      dispatch(Object.create(null));
-    },
-    [ dispatch ],
-  );
-  
-  return memoizedDispatch;
+    const memoizedDispatch = useCallback(() => {
+        dispatch(Object.create(null));
+    }, [dispatch]);
+
+    return memoizedDispatch;
 }
